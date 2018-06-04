6410243d2497997c8befc22bedb7dbc72f3e7039
Jeff Walker <walker.jeff.d@gmail.com>
2018-06-04 15:10:03 -0400

    add dev-model-perf

---
+++ r/dev-model-perf.R
@@ -0,0 +1,40 @@
+# evaluate model performance as function of number of crossings
+
+library(tidyverse)
+library(jsonlite)
+
+config <- read_json("config.json")
+
+crossings <- read_csv(
+  "csv/crossings-01040002.csv",
+  col_types = cols(
+    id = col_integer(),
+    x_coord = col_double(),
+    y_coord = col_double(),
+    lon = col_double(),
+    lat = col_double()
+  )
+) %>%
+  select(id, x = x_coord, y = y_coord)
+
+run_model <- function(n) {
+  graph.linkages(sample_n(crossings, size = n), source = config$tiles$dir)
+}
+
+df <- data_frame(
+  n = c(1, 5, 10, 20, 50, 100)
+) %>%
+  mutate(
+    results = map(n, run_model),
+    elapsed = map_dbl(results, "elapsed")
+  )
+
+df %>%
+  ggplot(aes(n, elapsed)) +
+  geom_point() +
+  geom_line()
+
+df %>%
+  ggplot(aes(n, elapsed/n)) +
+  geom_point() +
+  geom_line()
