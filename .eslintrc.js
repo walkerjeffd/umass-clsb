59f24f75cb76dcbb9456a73299c26a11f1ba33af
Jeff Walker <walker.jeff.d@gmail.com>
2018-07-18 17:58:59 -0400

    update packages, add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+      "import"
+  ],
+  "rules": {
+    "comma-dangle": "off",
+    "no-plusplus": "off",
+    "no-console": "off",
+    "no-param-reassign": "off"
+  }
+};
