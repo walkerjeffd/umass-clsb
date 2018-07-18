45dda4162c01cc6a155527b317622044f2fdc8ef
Jeff Walker <walker.jeff.d@gmail.com>
2018-07-18 18:00:01 -0400

    add graph lib tests

---
+++ test/graph.js
@@ -0,0 +1,72 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+const chai = require('chai');
+const jStat = require('jStat');
+const graph = require('../lib/graph/');
+const {
+  effects,
+  kernels,
+  nodes,
+  edges,
+  targets
+} = require('../lib/graph/test-data');
+
+const expect = chai.expect; // eslint-disable-line
+
+edges.forEach((e, i) => {
+  e.index = i;
+});
+
+describe('graph', function () {
+  describe('#test-data', function () {
+    it('should have node costs exceed node upgrades', function () {
+      const costSum = jStat.sum(nodes.map(d => d.cost));
+      const upgradeSum = jStat.sum(nodes.map(d => d.upgrades));
+      expect(costSum).to.be.above(upgradeSum);
+    });
+    it('should have one target', function () {
+      expect(targets).to.be.lengthOf(1);
+    });
+    it('should have one node with upgrades equal to 0', function () {
+      const targetNodes = nodes.filter(d => d.cost !== d.upgrades);
+      const targetNode = targetNodes[0];
+
+      expect(targetNodes).to.be.lengthOf(1);
+      expect(targetNode.upgrades).to.equal(0);
+    });
+  });
+
+  describe('#kernel()', function () {
+    const base = graph.kernel(targets, nodes, edges, nodes.map(d => d.cost));
+    const alt = graph.kernel(targets, nodes, edges, nodes.map(d => d.upgrades));
+    it('should return array with length equal to number of edges', function () {
+      expect(base).to.be.a('array').with.lengthOf(edges.length);
+    });
+    it('should return array of numbers without any NaN', function () {
+      base.every(d => expect(d).to.be.a('number').and.not.be.NaN);
+    });
+    it('should reproduce base kernel for test case', function () {
+      expect(base).to.have.lengthOf(kernels.base.length);
+      base.every((d, i) => expect(d).to.be.closeTo(kernels.base[i], 0.001));
+    });
+    it('should reproduce alt kernel for test case', function () {
+      expect(alt).to.have.lengthOf(kernels.alt.length);
+      alt.every((d, i) => expect(d).to.be.closeTo(kernels.alt[i], 0.001));
+    });
+  });
+
+  describe.skip('#linkages()', function () {
+    const out = graph.linkages(targets, nodes, edges);
+    it('should return object', function () {
+      expect(out).to.be.a('object');
+      expect(out).to.have.property('delta');
+      expect(out).to.have.property('effect');
+      expect(out).to.have.property('kernels');
+    });
+    it('should reproduce delta and effect for test case', function () {
+      expect(out.delta.total).to.be.closeTo(effects.delta.total, 0.001);
+      expect(out.effect.total).to.be.closeTo(effects.effect.total, 0.001);
+    });
+  });
+});
