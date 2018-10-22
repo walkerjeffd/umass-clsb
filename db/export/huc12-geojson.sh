dee5bf737bc2d081f672b63f171e2f5211b5125f
Jeff Walker <walker.jeff.d@gmail.com>
2018-10-22 16:24:48 -0400

    add huc12 geojson export script

---
+++ db/export/huc12-geojson.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Export huc12 polygons as geojson
+# usage: ./huc12-geojson.sh <output path>
+# example: ./huc12-geojson.sh ../../r/geojson/huc12.geojson
+
+set -eu
+
+FILE=$1
+
+. ../../config.sh
+
+# simplify tolerance in projected units (meters)
+
+echo -n Exporting "$FILE"...
+ogr2ogr -f "GeoJSON" "$FILE" "PG:dbname=$SHEDS_CLSB_DB_CONNSTRING" -t_srs EPSG:4326 -sql "select w.huc12, w.name, w.geom from wbdhu12 w where exists (select 1 from barriers b where st_intersects(w.geom, b.geom) limit 1);" -simplify 100
+echo done
--- db/export/huc8-geojson.sh
+++ db/export/huc8-geojson.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Export huc8 polygons as geojson
-# usage: ./huc8-geojson.sh <dbname> <output path>
-# example: ./huc8-geojson.sh clsb ../../r/geojson/huc8.geojson
+# usage: ./huc8-geojson.sh <output path>
+# example: ./huc8-geojson.sh ../../r/geojson/huc8.geojson
 
 set -eu
 
-DB=$1
-FILE=$2
+FILE=$1
+
+. ../../config.sh
 
 # simplify tolerance in projected units (meters)
 
-echo -n Exporting "$FILE" from "$DB"...
-ogr2ogr -f "GeoJSON" "$FILE" "PG:dbname=$DB" -t_srs EPSG:4326 -sql "select w.huc8, w.name, w.geom from wbdhu8 w where exists (select 1 from crossings c where st_intersects(w.geom, c.geom) limit 1);" -simplify 500
+echo -n Exporting "$FILE"...
+ogr2ogr -f "GeoJSON" "$FILE" "PG:dbname=$SHEDS_CLSB_DB_CONNSTRING" -t_srs EPSG:4326 -sql "select w.huc8, w.name, w.geom from wbdhu8 w where exists (select 1 from crossings c where st_intersects(w.geom, c.geom) limit 1);" -simplify 500
 echo done
