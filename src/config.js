b5c95ca83f8201755ac3ab266ae44e3430257459
Jeff Walker <walker.jeff.d@gmail.com>
2018-11-01 13:46:06 -0400

    add src/config

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    api: {
+      url: 'http://localhost:8000/'
+    }
+  },
+  production: {
+    api: {
+      url: 'http://ecosheds.org:8030/'
+    }
+  }
+};
