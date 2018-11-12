3095953f56ff9b0c94c150e511c44cad98ddb4ad
Jeff Walker <walker.jeff.d@gmail.com>
2018-11-12 11:03:50 -0500

    remove dev views from router

--- src/router.js
+++ src/router.js
@@ -31,31 +31,6 @@ export default new Router({
       path: '/builder',
       name: 'builder',
       component: () => import('./views/Builder.vue')
-    },
-    {
-      path: '/dev/map',
-      name: 'dev-map',
-      component: () => import('./dev/Map.vue')
-    },
-    {
-      path: '/dev/crossfilter-map',
-      name: 'dev-crossfilter-map',
-      component: () => import('./dev/CrossfilterMap.vue')
-    },
-    {
-      path: '/dev/crossfilter-dc',
-      name: 'dev-crossfilter-dc',
-      component: () => import('./dev/CrossfilterDc.vue')
-    },
-    {
-      path: '/dev/map-full',
-      name: 'dev-map-full',
-      component: () => import('./dev/MapFull.vue')
-    },
-    {
-      path: '/dev/map-full-tabs',
-      name: 'dev-map-full-tabs',
-      component: () => import('./dev/MapFullTabs.vue')
     }
   ],
 });
